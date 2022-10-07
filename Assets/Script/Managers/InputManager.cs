d370f9c6dd40c583f95409133b94704e2a2eed12
mallevondoning <malcolm.bergander@futuregames.nu>
2022-10-07 15:42:11 +0200

    added more folder sturcture and a start on a player controller

---
+++ Assets/Script/Managers/InputManager.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    public static float DirX { get; set; }
+    public static float DirY { get; set; } 
+}
